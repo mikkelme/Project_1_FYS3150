e3f91de419a2f74ced016521317a4fa9cf783b8d
Mikkel Metzsch Jensen <metzschjensen@gmail.com>
2020-08-27 16:07:29 +0200

    don't fuck with memory

--- 2b_Cpp/2b.cpp
+++ 2b_Cpp/2b.cpp
@@ -19,15 +19,66 @@ int main(int argc, char *argv[])
 {
   //Read file and exponent from command line
   int exponent; //Matrix size: exponent x exponent
-  string filename; //File contains X---X ????
-    if (argc <= 1) {
+  int a;
+  int b;
+  int c;
+  int n;
+  double h;
+
+
+    if (argc <= 3) {
       cout << "Bad Usage: \"" << argv[0] <<
-              "\" reads filename and max power 10^n as command line input" << endl;
+              "\" reads integers a, b, c and then max power 10^n as command line input" << endl;
           exit(1);
     }
     else {
-      filename = argv[1];
-      exponent = atoi(argv[2]);
+      a = atoi(argv[1]);
+      b = atoi(argv[2]);
+      c = atoi(argv[3]);
+      exponent = atoi(argv[4]);
+      n = pow(10, exponent);
+      h = 1./((double)n - 1.);
+    }
+
+    //Define arrays
+    double *A = new double [n];   //A = (double*)malloc(n*sizeof(double));
+    double *B = new double [n];   //B = (double*)malloc(n*sizeof(double));
+    double *C = new double [n];   //C = (double*)malloc(n*sizeof(double));
+    for (int i = 0; i < n; i++){
+      A[i] = a;
+      B[i] = b;
+      C[i] = c;
+    }
+
+    //Forward Sub
+    double *B_tilde = new double [n];
+    double *g_tilde = new double [n];
+    double *g = new double [n];
+    double *x = new double [n];
+
+    for (int i = 0; i < n; i++){
+      x[i] = (i)*h;
+      //cout << i << " " << x[i] << endl;
+      g[i] = pow(h,2)*f(x[i]);
+      B_tilde[i] = B[i] - A[i]*C[i-1]/B_tilde[i-1];
+      g_tilde[i] = g[i] - A[i]*g_tilde[i-1]/B_tilde[i-1];
+    }
+
+
+    //Backwards Sub
+    double *u = new double [n];
+    u[0] = 0.;
+    u[n-1] = 0.;
+    for (int i = n-1; i > 0; i--){
+      u[i-1] = g_tilde[i-1] - (C[i-1]*u[i])/B_tilde[i-1];
+
+    }
+
+
+    for (int i = 0; i < n; i++){
+      double RelativeError = fabs((analytic(x[i]) - u[i])/analytic(x[i]));
+      cout << RelativeError << endl;
+
     }
 
 
@@ -36,11 +87,9 @@ int main(int argc, char *argv[])
 
 
 
-
-
-
-
-
+    delete[] A;   //free(A);
+    delete[] B;   //free(B);
+    delete[] C;   //free(C)
 
   return 0;
 }
